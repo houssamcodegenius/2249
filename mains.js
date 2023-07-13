734e21528a00574c7ad0bc078bc3a029d342d2b5
houssamcodegenius <138686091+houssamcodegenius@users.noreply.github.com>
2023-07-13 22:37:00 +0100

    Add files via upload

--- mains.js
+++ mains.js
@@ -10,3 +10,18 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+const themeSwitchBtn = document.getElementById('theme-switch-btn');
+const themeIcon = document.getElementById('theme-icon');
+
+themeSwitchBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
+  } else {
+    themeIcon.classList.remove('fa-sun');
+    themeIcon.classList.add('fa-moon');
+  }
+});
